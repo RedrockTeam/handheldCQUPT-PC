d420fbb8189d32db621f52904ec47230405352e2
bibi <bibi@bibi.local>
2018-10-19 21:53:05 +0800

    那就这样吧

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import './App/animations.less'
-//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
