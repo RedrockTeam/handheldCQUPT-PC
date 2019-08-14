2c3f0a5e0db907c44b881f141b5342827070ec48
lin0102 <1262280723@qq.com>
2019-08-14 16:18:27 +0800

    临时取消移动端跳转

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -25,11 +25,11 @@ export default class App extends Component {
 		)
 	}
 	componentWillMount() {
-		function goPAGE() {
-			if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-				window.location.href="http://hongyan.cqupt.edu.cn/app/m/"
-			}
-		}
-		goPAGE()
+		// function goPAGE() {
+		// 	if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+		// 		window.location.href="http://hongyan.cqupt.edu.cn/app/m/"
+		// 	}
+		// }
+		// goPAGE()
 	}
 }
